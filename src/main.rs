bf8ca98000b39ea183542b5a6ea7d438d6ac19ec
Brieuc Desoutter <brieuc.desoutter@gmail.com>
2023-12-02 08:20:35 +0300

    Minor changes.

--- src/main.rs
+++ src/main.rs
@@ -13,35 +13,38 @@ fn part_one(input: &str) -> u32 {
 }
 
 // This function should return all digits even when digit words are chained, eg 'twoneighthree'
-fn find_next_digit(input: &str) -> (Option<char>, Option<&str>) {
+fn find_next_digit(input: &str) -> (Option<u32>, Option<&str>) {
     if input.is_empty() {
         (None, None)
     } else if input.chars().next().unwrap().is_ascii_digit() {
-        (Some(input.chars().next().unwrap()), Some(&input[1..]))
+        let d = input.chars().next().unwrap() as u32 - '0' as u32;
+        (Some(d), Some(&input[1..]))
+    } else if input.starts_with("zero") {
+        (Some(0), Some(&input[3..]))
     } else if input.starts_with("one") {
-        (Some('1'), Some(&input[2..]))
+        (Some(1), Some(&input[2..]))
     } else if input.starts_with("two") {
-        (Some('2'), Some(&input[2..]))
+        (Some(2), Some(&input[2..]))
     } else if input.starts_with("three") {
-        (Some('3'), Some(&input[4..]))
+        (Some(3), Some(&input[4..]))
     } else if input.starts_with("four") {
-        (Some('4'), Some(&input[3..]))
+        (Some(4), Some(&input[3..]))
     } else if input.starts_with("five") {
-        (Some('5'), Some(&input[4..]))
+        (Some(5), Some(&input[4..]))
     } else if input.starts_with("six") {
-        (Some('6'), Some(&input[2..]))
+        (Some(6), Some(&input[2..]))
     } else if input.starts_with("seven") {
-        (Some('7'), Some(&input[4..]))
+        (Some(7), Some(&input[4..]))
     } else if input.starts_with("eight") {
-        (Some('8'), Some(&input[4..]))
+        (Some(8), Some(&input[4..]))
     } else if input.starts_with("nine") {
-        (Some('9'), Some(&input[3..]))
+        (Some(9), Some(&input[3..]))
     } else {
         (None, Some(&input[1..]))
     }
 }
 
-fn parse_line(input: &str) -> Vec<char> {
+fn parse_line(input: &str) -> Vec<u32> {
     let mut digits = Vec::new();
     let mut next = find_next_digit(input);
     if next.0.is_some() {
@@ -62,10 +65,7 @@ fn part_two(input: &str) -> u32 {
         .lines()
         .map(parse_line)
         .map(|v| {
-            let mut s = String::new();
-            s.push(*v.first().unwrap());
-            s.push(*v.last().unwrap());
-            let num = s.parse::<u32>().unwrap();
+            let num = 10 * *v.first().unwrap() + *v.last().unwrap();
             println!(" -> {}", num);
             num
         })
@@ -85,6 +85,6 @@ mod tests {
     #[test]
     fn test_parse_line() {
         let digits = parse_line("twoneighthree");
-        assert_eq!(digits, &['2', '1', '8', '3']);
+        assert_eq!(digits, &[2, 1, 8, 3]);
     }
 }
