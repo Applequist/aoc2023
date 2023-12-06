9c3cf61b5e533fa2ad09cec4a52b191fe1e20d78
Brieuc Desoutter <brieuc.desoutter@gmail.com>
2023-12-06 10:36:29 +0300

    Day 3 part 2. Cleaned up.

--- src/bin/day3.rs
+++ src/bin/day3.rs
@@ -1,31 +1,46 @@
-use aoc2023::day3::{numbers, SymbolMap};
+use aoc2023::day3::{NumberMap, SymbolMap};
 
 fn main() {
     let input = include_str!("../../data/dec3.txt");
-    let symbols = SymbolMap::parse(input);
-    let num = numbers(input);
-
     let lines: Vec<_> = input.lines().collect();
-    let (line_count, col_count) = (lines.len(), lines.iter().map(|s| s.len()).max().unwrap());
-    let sum: u64 = num
+    let maps = (SymbolMap::new(&lines), NumberMap::new(&lines));
+    let part1 = part_one(&maps);
+    let part2 = part_two(&maps);
+    println!("Answer for Day 3:");
+    println!("\tpart 1: {part1}");
+    println!("\tpart 2: {part2}");
+}
+
+fn part_one((ref symbols, ref numbers): &(SymbolMap, NumberMap)) -> u64 {
+    numbers
+        .numbers
         .iter()
         .filter_map(|(r, (s, e), n)| {
+            let (line_count, col_count) = numbers.size;
             let min_row = if *r == 0 { 0 } else { *r - 1 };
             let max_row = if *r == line_count - 1 { *r } else { *r + 1 };
             let min_col = if *s == 0 { 0 } else { *s - 1 };
             let max_col = if *e == col_count - 1 { *e } else { *e + 1 };
             let symbols = symbols.query((min_row, max_row), (min_col, max_col));
             if !symbols.is_empty() {
-                // println!("PART NUMBER: {n}");
                 Some(n)
             } else {
-                println!("Not a part number: line {} number {}", r + 1, n);
-                (min_row..=max_row).for_each(|r| {
-                    println!("{}", &lines[r][min_col..=max_col]);
-                });
                 None
             }
         })
-        .sum();
-    println!("sum = {sum}");
+        .sum()
+}
+
+fn part_two((ref symbols, ref numbers): &(SymbolMap, NumberMap)) -> u64 {
+    symbols
+        .gears()
+        .filter_map(|((r, c), _)| {
+            let numbers = numbers.query((r, c));
+            if numbers.len() == 2 {
+                Some(numbers.iter().product::<u64>())
+            } else {
+                None
+            }
+        })
+        .sum()
 }
--- src/day3.rs
+++ src/day3.rs
@@ -1,32 +1,33 @@
-//! Idea: for each line, create the following:
+//! Part 1: for each line, create the following:
 //! - a list of symbol position
-//! - and a list of number + (start, end) positions.
+//! - and a list of number + line and (start, end) positions.
 //!
-//! Then for each number in line N, check if there is a symbol in line N-1, N and N+1 that is
-//! adjacent to it range, and if so add it to the sum of part number.
-//! To avoid summing the number more than once if it is adjacent to more than one symbols
-//! we first merge the symbols positions of all 3 lines (2 for first and last lines).
+//! Then for each number query the symbol in the number bounding box. If not empty sum it.
+//!
+//! Part 2: Build the same symbol and numbers. But this time query the number in the surround gear
+//! symbol, if exactly 2, multiply them and sum the result.
 
 /// A sparse symbol map: for each symbol, it stores the line and column indices (0 to N) in the
 /// input.
 #[derive(Debug, Clone, PartialEq)]
 pub struct SymbolMap {
-    symbols: Vec<(usize, usize)>,
+    size: (usize, usize),
+    symbols: Vec<((usize, usize), char)>,
 }
 
 impl SymbolMap {
-    pub fn parse(input: &str) -> Self {
-        let symbols = input
-            .lines()
+    pub fn new(lines: &[&str]) -> Self {
+        let size = (lines.len(), lines.iter().map(|s| s.len()).max().unwrap());
+
+        let symbols = lines
+            .iter()
             .enumerate()
             .flat_map(|(r, l)| {
-                println!("Extracting symbols from line {r}: '{l}'");
                 l.chars()
                     .enumerate()
                     .filter_map(|(c, a)| {
                         if a != '.' && !a.is_ascii_digit() {
-                            println!("\tsym '{a}' at ({r}, {c})");
-                            Some((r, c))
+                            Some(((r, c), a))
                         } else {
                             None
                         }
@@ -34,73 +35,111 @@ impl SymbolMap {
                     .collect::<Vec<_>>()
             })
             .collect();
-        SymbolMap { symbols }
+        SymbolMap { size, symbols }
     }
 
-    pub fn query(&self, rows: (usize, usize), columns: (usize, usize)) -> Vec<(usize, usize)> {
+    pub fn query(
+        &self,
+        rows: (usize, usize),
+        columns: (usize, usize),
+    ) -> Vec<((usize, usize), char)> {
         self.symbols
             .iter()
-            .filter(|(r, c)| *r >= rows.0 && *r <= rows.1 && *c >= columns.0 && *c <= columns.1)
+            .filter(|((r, c), _)| {
+                *r >= rows.0 && *r <= rows.1 && *c >= columns.0 && *c <= columns.1
+            })
             .cloned()
+            .collect()
+    }
+
+    pub fn gears<'a>(&'a self) -> impl Iterator<Item = ((usize, usize), char)> + 'a {
+        self.symbols.iter().filter(|((_, _), a)| *a == '*').cloned()
+    }
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct NumberMap {
+    pub size: (usize, usize),
+    pub numbers: Vec<(usize, (usize, usize), u64)>,
+}
+
+impl NumberMap {
+    pub fn new(lines: &[&str]) -> Self {
+        let size = (lines.len(), lines.iter().map(|s| s.len()).max().unwrap());
+        let numbers = lines
+            .iter()
+            .enumerate()
+            .flat_map(|(r, l)| {
+                let mut numbers = vec![];
+                let mut start = None;
+                for (c, a) in l.chars().enumerate() {
+                    if start.is_none() {
+                        if a.is_ascii_digit() {
+                            start = Some(c);
+                        }
+                    } else if !a.is_ascii_digit() {
+                        let start_ix = start.unwrap();
+                        start = None;
+                        let num = l[start_ix..c].parse::<u64>().unwrap();
+                        numbers.push((r, (start_ix, c - 1), num));
+                    } else {
+                        continue;
+                    }
+                }
+                if let Some(s) = start {
+                    let num = l[s..].parse::<u64>().unwrap();
+                    numbers.push((r, (s, l.len() - 1), num));
+                }
+                numbers
+            })
+            .collect();
+        Self { size, numbers }
+    }
+
+    pub fn query(&self, pos: (usize, usize)) -> Vec<u64> {
+        self.numbers
+            .iter()
+            .filter_map(|(l, (s, e), n)| {
+                if *l < pos.0 - 1 || *l > pos.0 + 1 || *e < pos.1 - 1 || *s > pos.1 + 1 {
+                    None
+                } else {
+                    Some(*n)
+                }
+            })
             .collect()
     }
 }
 
-pub fn numbers(input: &str) -> Vec<(usize, (usize, usize), u64)> {
-    input
-        .lines()
-        .enumerate()
-        .flat_map(|(r, l)| {
-            println!("Extracting numbers from line {r}: '{l}'");
-            let mut numbers = vec![];
-            let mut start = None;
-            for (c, a) in l.chars().enumerate() {
-                if start.is_none() {
-                    if a.is_ascii_digit() {
-                        start = Some(c);
-                    }
-                } else if !a.is_ascii_digit() {
-                    let start_ix = start.unwrap();
-                    start = None;
-                    let num = l[start_ix..c].parse::<u64>().unwrap();
-                    numbers.push((r, (start_ix, c - 1), num));
-                    let s = if start_ix == 0 { 0 } else { start_ix - 1 };
-                    println!("\tnum {} ('{}')", num, &l[s..=c]);
-                } else {
-                    continue;
-                }
-            }
-            if let Some(s) = start {
-                let num = l[s..].parse::<u64>().unwrap();
-                numbers.push((r, (s, l.len() - 1), num));
-                let ctx_s = if s == 0 { 0 } else { s - 1 };
-                println!("\tnum {} ('{}')", num, &l[ctx_s..]);
-            }
-            numbers
-        })
-        .collect()
-}
-
 #[cfg(test)]
 mod tests {
-    use crate::day3::{numbers, SymbolMap};
+    use crate::day3::{NumberMap, SymbolMap};
 
     #[test]
     fn test_symbolmap() {
         let input = include_str!("../data/dec3_sample.txt");
+        let lines = input.lines().collect::<Vec<_>>();
         assert_eq!(
-            SymbolMap::parse(input),
+            SymbolMap::new(&lines),
             SymbolMap {
-                symbols: vec![(1, 3), (3, 6), (4, 3), (5, 5), (8, 3), (8, 5)]
+                size: (10, 10),
+                symbols: vec![
+                    ((1, 3), '*'),
+                    ((3, 6), '#'),
+                    ((4, 3), '*'),
+                    ((5, 5), '+'),
+                    ((8, 3), '$'),
+                    ((8, 5), '*')
+                ]
             }
         );
     }
 
     #[test]
-    fn test_numbers() {
+    fn test_numbermap() {
         let input = include_str!("../data/dec3_sample.txt");
+        let lines = input.lines().collect::<Vec<_>>();
         assert_eq!(
-            numbers(input),
+            NumberMap::new(&lines).numbers,
             vec![
                 (0, (0, 2), 467),
                 (0, (5, 7), 114),
