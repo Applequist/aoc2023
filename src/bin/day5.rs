12469e4b800757abeca233c5c40915ecc62f0dc3
Brieuc Desoutter <brieuc.desoutter@gmail.com>
2023-12-09 12:10:16 +0300

    Day 5 part 2.

--- src/bin/day5.rs
+++ src/bin/day5.rs
@@ -1,14 +1,14 @@
-use aoc2023::day5::{seeds_and_maps, IntervalMap};
+use aoc2023::day5::{seeds_and_maps, Interval, IntervalMap};
 
 fn main() {
-    let input = include_str!("../../data/dec5_sample.txt");
+    let input = include_str!("../../data/dec5.txt");
     let ((seeds, seed_ranges), maps) = seeds_and_maps(input).unwrap().1;
 
     let part1 = part_one((&seeds, &maps));
-    // let part2 = part_two((&seed_ranges, &maps));
+    let part2 = part_two((&seed_ranges, &maps));
     println!("Answer for Day 5:");
     println!("\tpart 1: {part1}");
-    // println!("\tpart 2: {part2}");
+    println!("\tpart 2: {part2}");
 }
 
 fn part_one((seeds, maps): (&[u64], &[IntervalMap])) -> u64 {
@@ -25,6 +25,23 @@ fn part_one((seeds, maps): (&[u64], &[IntervalMap])) -> u64 {
         .unwrap()
 }
 
-// fn part_two((seed_ranges, maps): (&[Rng], &[RangeMap])) -> u64 {
-//     0
-// }
+fn part_two((seed_ranges, maps): (&[Interval], &[IntervalMap])) -> u64 {
+    let mut unmapped = Vec::new();
+    unmapped.extend_from_slice(seed_ranges);
+    unmapped.sort_by(|a, b| a.min.partial_cmp(&b.min).unwrap());
+    for (ix, m) in maps.iter().enumerate() {
+        println!("Map {ix}:\n{m}");
+        println!("-------------------------------------------");
+        let mut next_unmapped = unmapped
+            .iter()
+            .flat_map(|&r| m.map_interval(r).into_iter())
+            .collect::<Vec<_>>();
+        next_unmapped.sort_by(|a, b| a.min.partial_cmp(&b.min).unwrap());
+        println!("Map {ix}: output = {:?}", next_unmapped);
+        unmapped = next_unmapped;
+    }
+
+    let min_interval = unmapped.first().unwrap();
+    println!("{min_interval}");
+    unmapped.first().map(|i| i.min).unwrap()
+}
--- src/day5.rs
+++ src/day5.rs
@@ -1,20 +1,104 @@
 use nom::{
-    branch::alt,
     bytes::complete::{is_not, tag},
     character::complete::{char, digit1, line_ending, space1},
-    combinator::{eof, map_res},
-    multi::{count, separated_list1},
-    sequence::{delimited, preceded, terminated, tuple},
+    combinator::map_res,
+    multi::separated_list1,
+    sequence::tuple,
     IResult, Parser,
 };
+use std::{fmt::Display, iter::Iterator};
 
-/// An interval (src, len) = src..src+len
+/// An **non-empty** closed interval (src, len) containing interger >= src and < src + len.
 #[derive(Debug, Clone, Copy, PartialEq)]
-pub struct Interval(u64, u64);
+pub struct Interval {
+    pub min: u64,
+    pub max: u64,
+}
 
 impl Interval {
+    pub fn min_max(min: u64, max: u64) -> Self {
+        assert!(max >= min);
+        Self { min, max }
+    }
+
+    pub fn start_len(start: u64, len: u64) -> Self {
+        assert!(len > 0);
+        Self {
+            min: start,
+            max: start + len - 1,
+        }
+    }
+
+    #[inline]
+    pub fn len(&self) -> u64 {
+        self.max + 1 - self.min
+    }
+
+    #[inline]
+    pub fn lt(&self, v: u64) -> bool {
+        self.max < v
+    }
+
+    #[inline]
+    pub fn gt(&self, v: u64) -> bool {
+        v < self.min
+    }
+
+    /// Return `true` if this interval contains v.
+    #[inline]
     pub fn contains(&self, v: u64) -> bool {
-        self.0 <= v && v < self.0 + self.1
+        self.min <= v && v <= self.max
+    }
+
+    #[inline]
+    pub fn overlap(&self, other: Interval) -> bool {
+        self.max >= other.min && self.min <= other.max
+    }
+
+    #[inline]
+    pub fn touch(&self, other: Interval) -> bool {
+        self.max + 1 == other.min || other.max + 1 == self.min
+    }
+
+    #[inline]
+    pub fn include(&self, other: Interval) -> bool {
+        self.min <= other.min && other.max <= self.max
+    }
+
+    /// Split this interval into a *left* interval of values less than v and
+    /// a *right* interval of values greater than or equal to v.
+    pub fn split_before(&self, v: u64) -> (Option<Interval>, Option<Interval>) {
+        if v <= self.min {
+            (None, Some(*self))
+        } else if v > self.max {
+            (Some(*self), None)
+        } else {
+            (
+                Some(Interval::min_max(self.min, v - 1)),
+                Some(Interval::min_max(v, self.max)),
+            )
+        }
+    }
+
+    /// Split this interval into a *left* interval of values less than or equal to v and
+    /// a *right* interval of values greater than v.
+    pub fn split_after(&self, v: u64) -> (Option<Interval>, Option<Interval>) {
+        if v >= self.max {
+            (Some(*self), None)
+        } else if v < self.min {
+            (None, Some(*self))
+        } else {
+            (
+                Some(Interval::min_max(self.min, v)),
+                Some(Interval::min_max(v + 1, self.max)),
+            )
+        }
+    }
+}
+
+impl Display for Interval {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "[{}..{}]", self.min, self.max)
     }
 }
 
@@ -28,9 +112,23 @@ pub struct Mapping {
 }
 
 impl Mapping {
+    pub fn source(&self) -> Interval {
+        self.source
+    }
+
+    pub fn destination(&self) -> Interval {
+        Interval::start_len(self.dst, self.source.len())
+    }
+
     pub fn map(&self, v: u64) -> u64 {
         assert!(self.source.contains(v));
-        self.dst + (v - self.source.0)
+        self.dst + (v - self.source.min)
+    }
+}
+
+impl Display for Mapping {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{} -> {}", self.source, self.destination())
     }
 }
 
@@ -38,7 +136,7 @@ impl From<(u64, u64, u64)> for Mapping {
     /// Create a Mapping from (dst, src, len).
     fn from(value: (u64, u64, u64)) -> Self {
         Self {
-            source: Interval(value.1, value.2),
+            source: Interval::start_len(value.1, value.2),
             dst: value.0,
         }
     }
@@ -50,6 +148,19 @@ pub struct IntervalMap {
 }
 
 impl IntervalMap {
+    pub fn new(mut mappings: Vec<Mapping>) -> Self {
+        mappings.as_mut_slice().sort_by(|a, b| {
+            let a_min = a.source.min;
+            let b_min = b.source.min;
+            a_min.partial_cmp(&b_min).unwrap()
+        });
+        assert!(mappings
+            .as_slice()
+            .windows(2)
+            .all(|w| w[0].source.lt(w[1].source.min)));
+        Self { mappings }
+    }
+
     pub fn map(&self, n: u64) -> u64 {
         let rng_mapping = self.mappings.iter().find(|&m| m.source.contains(n));
         if let Some(m) = rng_mapping {
@@ -57,6 +168,56 @@ impl IntervalMap {
         } else {
             n
         }
+    }
+
+    pub fn map_interval(&self, interval: Interval) -> Vec<Interval> {
+        print!("{}", interval);
+        let mut unmapped = Some(interval);
+        let mut mapped = vec![];
+        for m in self.mappings.iter() {
+            if m.source.lt(unmapped.unwrap().min) {
+                continue;
+            }
+            if m.source.gt(unmapped.unwrap().max) {
+                mapped.push(unmapped.unwrap());
+                unmapped = None;
+                break;
+            }
+            let (l, r) = unmapped.unwrap().split_before(m.source.min);
+            if let Some(i) = l {
+                mapped.push(i);
+            }
+            unmapped = r;
+            if unmapped.is_none() {
+                break;
+            }
+
+            let (l, r) = unmapped.unwrap().split_after(m.source.max);
+            if let Some(i) = l {
+                mapped.push(Interval::start_len(m.map(i.min), i.len()));
+            }
+            unmapped = r;
+            if unmapped.is_none() {
+                break;
+            }
+        }
+
+        // if all mapping are less than the interval push as is.
+        if let Some(i) = unmapped {
+            mapped.push(i);
+        }
+
+        println!(" -> {:?}", mapped);
+        mapped
+    }
+}
+
+impl Display for IntervalMap {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        for m in &self.mappings {
+            write!(f, "{m}\n")?;
+        }
+        Ok(())
     }
 }
 
@@ -74,7 +235,9 @@ fn seeds(input: &str) -> IResult<&str, (Vec<u64>, Vec<Interval>)> {
         .map(|(_, _, n, _)| {
             (
                 n.clone(),
-                n.chunks_exact(2).map(|s| Interval(s[0], s[1])).collect(),
+                n.chunks_exact(2)
+                    .map(|s| Interval::start_len(s[0], s[1]))
+                    .collect(),
             )
         })
         .parse(input)
@@ -94,7 +257,7 @@ fn mapping(input: &str) -> IResult<&str, Mapping> {
 
 fn interval_map(input: &str) -> IResult<&str, IntervalMap> {
     tuple((header, separated_list1(line_ending, mapping)))
-        .map(|(_, m)| IntervalMap { mappings: m })
+        .map(|(_, m)| IntervalMap::new(m))
         .parse(input)
 }
 
@@ -116,7 +279,13 @@ mod tests {
     fn test_seeds() {
         assert_eq!(
             seeds("seeds: 1 2 3 4\n"),
-            Ok(("", (vec![1, 2, 3, 4], vec![Interval(1, 2), Interval(3, 4)])))
+            Ok((
+                "",
+                (
+                    vec![1, 2, 3, 4],
+                    vec![Interval::start_len(1, 2), Interval::start_len(3, 4)]
+                )
+            ))
         );
     }
 
@@ -127,7 +296,7 @@ mod tests {
             Ok((
                 "",
                 Mapping {
-                    source: Interval(98, 2),
+                    source: Interval::start_len(98, 2),
                     dst: 50
                 }
             ))
@@ -140,28 +309,91 @@ mod tests {
             interval_map("seed-to-soil map:\n1 2 3\n4 5 6"),
             Ok((
                 "",
-                IntervalMap {
-                    mappings: vec![
-                        Mapping {
-                            source: Interval(2, 3),
-                            dst: 1
-                        },
-                        Mapping {
-                            source: Interval(5, 6),
-                            dst: 4
-                        },
-                    ]
-                }
+                IntervalMap::new(vec![
+                    Mapping {
+                        source: Interval::start_len(2, 3),
+                        dst: 1
+                    },
+                    Mapping {
+                        source: Interval::start_len(5, 6),
+                        dst: 4
+                    },
+                ])
             ))
         );
     }
+
     #[test]
     fn test_interval_contains() {
-        let a = Interval(10, 2);
+        let a = Interval::start_len(10, 2);
         assert!(!a.contains(9));
         assert!(a.contains(10));
         assert!(a.contains(11));
         assert!(!a.contains(12));
+    }
+
+    #[test]
+    fn test_interval_overlap() {
+        let a = Interval::start_len(1, 3);
+        let b = Interval::start_len(3, 5);
+        let c = Interval::start_len(4, 2);
+        let d = Interval::start_len(6, 3);
+
+        assert!(a.overlap(b));
+        assert!(!a.overlap(c));
+        assert!(b.overlap(c));
+        assert!(!c.overlap(d));
+    }
+
+    #[test]
+    fn test_interval_touch() {
+        let a = Interval::min_max(1, 2); // [1, 2]
+        let b = Interval::min_max(3, 3); // [3]
+        let c = Interval::min_max(4, 5); // [4, 5]
+        assert!(a.touch(b));
+        assert!(b.touch(c));
+    }
+
+    #[test]
+    fn test_interval_split_before() {
+        let a = Interval::min_max(1, 10);
+        assert_eq!(a.split_before(1), (None, Some(a)));
+        assert_eq!(
+            a.split_before(5),
+            (
+                Some(Interval::min_max(1, 4)),
+                Some(Interval::min_max(5, 10))
+            )
+        );
+        assert_eq!(
+            a.split_before(10),
+            (
+                Some(Interval::min_max(1, 9)),
+                Some(Interval::min_max(10, 10))
+            )
+        );
+        assert_eq!(a.split_before(11), (Some(a), None));
+    }
+
+    #[test]
+    fn test_interval_split_after() {
+        let a = Interval::min_max(1, 10);
+        assert_eq!(a.split_after(0), (None, Some(a)));
+        assert_eq!(
+            a.split_after(1),
+            (
+                Some(Interval::min_max(1, 1)),
+                Some(Interval::min_max(2, 10))
+            )
+        );
+        assert_eq!(
+            a.split_after(5),
+            (
+                Some(Interval::min_max(1, 5)),
+                Some(Interval::min_max(6, 10))
+            )
+        );
+        assert_eq!(a.split_after(10), (Some(a), None));
     }
 
     #[test]
@@ -173,11 +405,31 @@ mod tests {
 
     #[test]
     fn test_intervalmap_map() {
-        let rng = IntervalMap {
-            mappings: vec![Mapping::from((50, 98, 2)), Mapping::from((52, 50, 48))],
-        };
+        let rng = IntervalMap::new(vec![
+            Mapping::from((50, 98, 2)),
+            Mapping::from((52, 50, 48)),
+        ]);
         assert_eq!(rng.map(10), 10);
         assert_eq!(rng.map(50), 52);
         assert_eq!(rng.map(98), 50);
+    }
+
+    #[test]
+    fn test_intervalmap_map_interval() {
+        let map = IntervalMap::new(vec![
+            Mapping::from((50, 98, 2)),  // [98, 99] -> [50, 51]
+            Mapping::from((52, 50, 48)), // [50..97] -> [52..100]
+        ]);
+        let a = Interval::min_max(10, 15);
+        assert_eq!(map.map_interval(a), vec![a]);
+
+        let b = Interval::min_max(60, 97);
+        assert_eq!(map.map_interval(b), vec![Interval::min_max(62, 99)]);
+
+        let c = Interval::min_max(95, 99);
+        assert_eq!(
+            map.map_interval(c),
+            vec![Interval::min_max(97, 99), Interval::min_max(50, 51),]
+        )
     }
 }
